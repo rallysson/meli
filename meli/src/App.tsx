004c5de77a35fc6ca108ae4809e7725ec6560253
rallysson <rallysson.diogo@acct.global>
2021-03-09 00:50:31 -0300

    feat(meli): adiciona Header ao App

--- meli/src/App.tsx
+++ meli/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+
+import Header from './components/Header';
+
 import './App.scss';
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+     <Header />   
+    </>
   );
 }
 
